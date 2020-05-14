d80a74028ba5fbfbe4d27f13423965b7bd2c736a
dariastarica <dariastarica2000@gmail.com>
2020-05-14 22:05:37 +0300

    Commit untracked files

---
+++ PHP/TEST.php
@@ -0,0 +1,21 @@
+<?php
+$connectionString = sprintf("mysql:host=%s;dbname=%s","meqserver.mysql.database.azure.com","test");
+try{
+    $pdoconnection = new PDO($connectionString,"meqadmin@meqserver","MEq1234@000000");
+}catch (PDOException $exception){
+    echo $exception,PHP_EOL;
+}
+$insertvalue =3;
+
+if($pdoconnection != null){
+    $statementemnString = "select * from testtable";
+    $statement=$pdoconnection->prepare($statementemnString);
+    $statement->bindParam(":value",$insertvalue);
+    $statement->execute();
+    while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        //echo $row->id,$row->value, PHP_EOL;
+        print_r($row);
+    }
+    $pdoconnection=null; //disconnect
+}
+
