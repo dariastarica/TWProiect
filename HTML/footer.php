92f6c7a98af0e99d1ba989b79dbd24a49be55d0a
”MariaIstrate06” <mistrate06@gmail.com>
2020-05-19 13:06:42 +0300

    -news page php updated for test

---
+++ HTML/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <footer>
+        <div class="footer-container">
+            <button type="button" onclick="location.href = 'People/Maria.php' ">Maria Istrate</button>
+            <button type="button" onclick="location.href = 'People/Daria.php' ">Daria Stărică</button>
+            <button type="button" onclick="location.href = 'People/Tudor.php' ">Tudor Mihăiuc</button>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
---
+++ HTML/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <title> MEq </title>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="../CSS/Front+flex.css">
+</head>
+<body>
+<div class="topnav">
+    <a class="active" href="Front+flex.php">Home</a>
+    <a href="News.php">News</a>
+    <a href="Categories.php">Categories</a>
+    <a href="Contact.php">Contact</a>
+    <div class="login-container">
+    </div>
+</div>
