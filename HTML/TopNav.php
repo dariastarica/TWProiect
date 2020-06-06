661ef07331675b98f71ebc7b9438e755dac97e21
Tudor Mihaiuc <tudormihaiuc@gmail.com>
2020-06-06 20:38:25 +0300

    Alt fix la login + tre sa apesi de 2 ori pe logout ups :(

--- HTML/TopNav.php
+++ HTML/TopNav.php
@@ -3,13 +3,17 @@ if ($_SESSION['logged'] == false) {
     echo '<form action="" method="post"> 
                     <input type="submit" name="login" value="Login"/>
                   </form>';
-    if (isset($_POST['login'])) {
-        header("Location: ./Login.php");
-    }
 } else {
     echo '<form action="" method="post"> 
                     <input type="submit" name="logout" value="Log out"/>
                   </form>';//maria fa css la formul asta
-    $_SESSION['logged'] = false;
-    //trim($_SESSION['user_id']);
 }
+if (isset($_POST['login'])) {
+    header("Location: ./Login.php");
+} else
+    if (isset($_POST['logout'])) {
+        $_SESSION['logged'] = false;
+        trim($_SESSION['user_id']);
+    }
+    // tre sa apesi de doua ori ca sa iesi
+
