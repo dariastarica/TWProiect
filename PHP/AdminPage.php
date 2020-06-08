e5c689996a278278f9792e205c65741ca9c39b4a
dariastarica <dariastarica2000@gmail.com>
2020-06-08 23:45:51 +0300

    ShowUsers in Admin

--- PHP/AdminPage.php
+++ PHP/AdminPage.php
@@ -1,3 +1,68 @@
 <?php
+session_start();
+?>
+<html>
 
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <title> MEq </title>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="../CSS/Front+flex.css">
+</head>
 
+<body>
+<div class="topnav">
+    <a class="active" href="index.php">Home</a>
+    <a href="News.php">News</a>
+    <a href="Categories.php">Categories</a>
+    <a href="Contact.php">Contact</a>
+    <div class="login-container">
+        <?php
+        include 'TopNav.php';
+        ?>
+    </div>
+</div>
+
+lista useri
+delete useri
+
+<div>
+    <div id="usersList"><b>Users will be listed here...</b></div>
+</div>
+
+<script>
+  function showUsers() {
+          let xmlhttp = new XMLHttpRequest();
+      console.log("plm");
+          xmlhttp.onreadystatechange = function() {
+              if (this.readyState === 4 && this.status === 200) {
+                  console.log(this.responseText);
+                  document.getElementById("usersList").innerHTML = this.responseText;
+              }
+          };
+          xmlhttp.open("GET","./AdminPageController.php",true);
+          xmlhttp.send();
+      console.log("sent");
+      }
+
+  showUsers();
+</script>
+
+<div class="footer">
+    <footer>
+        <div class="footer-container">
+            <button type="button" onclick="location.href = 'People/Maria.php' ">Maria Istrate</button>
+            <button type="button" onclick="location.href = 'People/Daria.php' ">Daria Stărică</button>
+            <button type="button" onclick="location.href = 'People/Tudor.php' ">Tudor Mihăiuc</button>
+        </div>
+    </footer>
+</div>
+
+<!--<script> showUsers(); </script>-->
+</body>
+
+</html>
--- PHP/AdminPageController.php
+++ PHP/AdminPageController.php
@@ -1,2 +1,33 @@
 <?php
+include 'DatabaseConnection.php';
+$sql="SELECT user_id, user_name, user_email, user_date, user_level FROM users";
+$statement = $pdoconnection->prepare($sql);
+$statement->execute();
+
+$errors = array();
+echo "<table>
+<tr>
+<th>User ID</th>
+<th>Username</th>
+<th>User Email</th>
+<th>SignUp Date</th>
+<th>UserLevel</th>
+</tr>";
+if($statement->rowCount()>0) {
+    while ($row = $statement->fetch(PDO::FETCH_OBJ)) {
+        echo '<tr>';
+
+        echo "<td>" . $row->user_id . "</td>";
+        echo "<td>" . $row->user_name . "</td>";
+        echo "<td>" . $row->user_email . "</td>";
+        echo "<td>" . $row->user_date . "</td>";
+        echo "<td>" . $row->user_level . "</td>";
+        echo '<td><button type="button" class="add-eq-btn"> Delete User </button></td>';
+
+        echo '</tr>';
+    }
+
+}
+
+echo "</table>";
 
